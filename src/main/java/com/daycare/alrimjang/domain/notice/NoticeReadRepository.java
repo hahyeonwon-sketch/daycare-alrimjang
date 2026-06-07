14a26ffb15c14514fbb48258f0ed81c6e70ff159
hahyeonwon-sketch <hahyeonwon@gmail.com>
2026-06-07 14:40:45 +0900

    [#4] 학부모 알림장 서비스 메서드 추가

---
+++ src/main/java/com/daycare/alrimjang/domain/notice/NoticeReadRepository.java
@@ -0,0 +1,6 @@
+package com.daycare.alrimjang.domain.notice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeReadRepository extends JpaRepository<NoticeRead, Long> {
+}
